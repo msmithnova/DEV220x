7d6a82910c9a4dc15da7e47b56ef13e5d8184003
Michael Smith <mikesmithnova@gmail.com>
2018-02-13 03:11:57 -0400

    Declare Angular Controller

--- Mod1Lab/js/script.js
+++ Mod1Lab/js/script.js
@@ -1,1 +1,10 @@
+// Declare angular app
 var helloWorldApp = angular.module('helloWorldApp', []);
+
+// Declare angular controller
+helloWorldApp.controller('firstController', [
+    '$scope',
+    function($scope) {
+        $scope.appName = 'An App Name';
+    }
+]);
