3ffee34e1bb753a2afc816981793b89c3a2ab99a
Michael Smith <mikesmithnova@gmail.com>
2018-02-15 00:54:18 -0400

    Angular $resource example

---
+++ Mod4Lab/app/gitHubService.js
@@ -0,0 +1,23 @@
+angular.module('gitHubService', ['ngResource']).
+    factory('gitHub', [
+        '$resource',
+        function ($resource) {
+            return $resource('https://api.github.com/:action/angular/:id',
+            {
+                action: '@action',
+                id: '@id'
+            },
+            {
+                getAll: {
+                    method: 'GET',
+                    isArray: true,
+                    params: { action: 'orgs', id: 'repos' }
+                },
+                getDetail: {
+                    method: 'GET',
+                    params: { action: 'repos' }
+                },
+            });
+        }
+    ]
+);
--- Mod4Lab/app/labController.js
+++ Mod4Lab/app/labController.js
@@ -1,6 +1,6 @@
 app.controller('labController', [
-    '$scope', '$timeout', '$q', '$http',
-    function ($scope, $timeout, $q, $http) {
+    '$scope', '$timeout', '$q', '$http', 'gitHub',
+    function ($scope, $timeout, $q, $http, gitHub) {
         $scope.model = {
             number: 0,
             result: 'Ready'
@@ -9,20 +9,11 @@ app.controller('labController', [
         $scope.getRepos = getRepos;
         $scope.loadDetail = loadDetail;
         function getRepos() {
-            $http.get('https://api.github.com/orgs/angular/repos')
-            .then(function(response) {
-                $scope.model.repos = response.data;
-            }, function(response) {
-                $scope.model.repos = 'Error: ' + repsonse.data.message;
-            });
+            $scope.model.repos = gitHub.getAll();
         }
         function loadDetail(name) {
-            var url = 'https://api.github.com/repos/angular/' + name;
-            $http.get(url).then(function(response) {
-                $scope.model.detail = response.data;
-            }, function(response) {
-                $scope.model.detail = {error: true, message: 'Error: ' + response.data.message};
-            });
+            $scope.model.detail = null;
+            $scope.model.detail = gitHub.getDetail({id: name});
         }
         function checkOddNumber(input) {
             $scope.model.result = 'Working...';
